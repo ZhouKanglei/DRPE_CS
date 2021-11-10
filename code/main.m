afc2c4223d29324adccc0d4999b1696e3cea7dfa
ZhouKanglei <47837939+ZhouKanglei@users.noreply.github.com>
2021-11-10 11:44:07 +0800

    update fig_5

--- code/main.m
+++ code/main.m
@@ -1,13 +1,21 @@
-posibility = 0;
-g = 0.04;
-p = 0.875;
-for i = 1:40
-%     posibility = posibility + 0.005;
-%     p = i/8;
-    g = g + 0.002;
-    save('keys/tamp.mat', 'posibility');
-    save('keys/g.mat', 'g');
-    save('keys/p.mat', 'p');
+posibility = 0;% Ratio of tamper
+g = 0;         % Ratio of noisy param
+rate_crop = 0; % Ratio of cropping
+% for i = 1 : 5
+%     disp('---------------------------------');
+%     test;
+%     ans_name = ['ans/omp_sampling_', num2str(p), '_noise_', num2str(g),'_tamper_', num2str(posibility),'.mat'];
+%     save(ans_name);
+%     disp(['Save answer to ', ans_name]);
+%     g = g + 0.002;
+% end
+
+
+for i = 1 : 7
+    disp('---------------------------------');
+    p = i/8;
     test;
-    save(['ans/omp_sampling_', num2str(p), '_noise_', num2str(g),'_tamper_', num2str(posibility),'.mat']);
+    ans_name = ['ans/omp_sampling_', num2str(p), '_noise_', num2str(g),'_tamper_', num2str(posibility),'.mat'];
+    save(ans_name);
+    disp(['Save answer to ', ans_name]);
 end
