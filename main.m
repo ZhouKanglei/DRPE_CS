9710ddf450b9ada1c7460ac1712c8b373dbae0da
ZhouKanglei <47837939+ZhouKanglei@users.noreply.github.com>
2021-12-13 17:15:29 +0800

    Ren
    Rename the name of main function.

--- main.m
+++ main.m
@@ -7,12 +7,12 @@ posibility = 0;% Ratio of tamper
 g = 0;         % Ratio of noisy param
 rate_crop = 0; % Ratio of cropping
 p = 7 / 8;     % Ratio of sampling
-method = 'omp';% If method is set to cvx, then please wati for a long time.
+method = 'omp';% If method is set to `cvx`, then please wati for a long time.
 
 for i = 1 : 15
     disp('---------------------------------');
     p = i / 16;
-    test;
+    exp_main();
     ans_name = ['ans/', method, '_sampling-', num2str(p), '_noise-', num2str(g),...
         '_tamper-', num2str(posibility), '_crop-', num2str(rate_crop), '.mat'];
     save(ans_name);
